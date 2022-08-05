60f3f244038eb2c3e1e3ff61afd127d6bfa17fc0
sajjad <faniam321@gmail.com>
2022-08-05 13:48:37 +0430

    complete register user

--- users/serializer.py
+++ users/serializer.py
@@ -1,32 +1,21 @@
 from rest_framework import serializers
 from .models import User
-from phonenumber_field.modelfields import PhoneNumberField
 
 
 class RegisterUserSerializer(serializers.Serializer):
     full_name = serializers.CharField(max_length=200)
-    phonenumber = PhoneNumberField()
+    phone_number = serializers.CharField(max_length=13)
     password = serializers.CharField(max_length=20, write_only=True)
-    type = serializers.CharField(max_length=20)
 
-    def validate_phonenumber(self, value):
-        if User.objects.filter(phonenumber=value).exists():
+    def validate_phone_number(self, value):
+        if User.objects.filter(phone_number=value).exists():
             raise serializers.ValidationError(
                 'User with this phonenumber already exists')
         return value
 
-    def validate_type(self, value):
-        if value not in ['user', 'operator']:
-            raise serializers.ValidationError(
-                'Type must be either user or operator')
-        return value
-
     def create(self, validated_data):
-        type = validated_data.pop('type')
-        if type == 'user':
-            user = User.objects.create_user(**validated_data)
-        else:
-            user = User.objects.create_superuser(**validated_data)
-        user.set_password(validated_data['password'])
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
         user.save()
         return user

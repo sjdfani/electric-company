3f3ec56ec71409585fce88d36830572352fcb1d0
sajjad <faniam321@gmail.com>
2022-08-05 12:04:48 +0430

    complete manager/model

---
+++ users/manager.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+import uuid
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('User should have a phone number')
+        if not password:
+            raise ValueError('User should have a password')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        user = self.create_user(phone_number, password, **extra_fields)
+        user.api_key = uuid.uuid4().hex
+        user.save()
+        return user
--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+from django.utils.translation import ugettext_lazy as _
+from .manager import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    full_name = models.CharField(
+        max_length=200, verbose_name=_('Full name'), null=True)
+    phone_number = PhoneNumberField(
+        unique=True, verbose_name=_('Phone number'))
+    national_code = models.CharField(
+        max_length=10, verbose_name=_('National code'))
+    province = models.CharField(max_length=200, verbose_name=_('Province'))
+    city = models.CharField(max_length=200, verbose_name=_('City'))
+    postal_code = models.CharField(
+        max_length=100, verbose_name=_('Postal code'))
+    address = models.TextField(verbose_name=_('Address'))
+    api_key = models.CharField(max_length=200, verbose_name=_(
+        'API key'), blank=True, null=True)
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['full_name']
+    username = None
+    first_name = None
+    last_name = None
+    objects = UserManager()
+
+    def __str__(self):
+        return str(self.phone_number)

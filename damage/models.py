489110b1731caacf57cb8fbc969bdad3affca2ca
sajjad <faniam321@gmail.com>
2022-08-21 17:47:04 +0430

    complete damage

--- damage/models.py
+++ damage/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+import uuid
 
-# Create your models here.
+
+class Status(models.IntegerChoices):
+    TODO = (1, _('TODO'))
+    IN_PROGRESS = (2, _('In progress'))
+    DONE = (3, _('Done'))
+    FAILURE = (4, _('Failure'))
+    LACK_OF_SUFFICIENT_INFORMATION = (5, _('Lack of sufficient information'))
+
+
+def uuid_hex():
+    return uuid.uuid4().hex
+
+
+class TypeOfDamage(models.Model):
+    title = models.CharField(max_length=200, verbose_name=_('Type of damage'))
+
+    def __str__(self):
+        return self.title
+
+
+class Operator(models.Model):
+    full_name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.full_name
+
+
+class DamageReport(models.Model):
+    unique_id = models.CharField(
+        default=uuid_hex, max_length=500, editable=False, unique=True)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE,
+                             related_name='damage_reports', verbose_name=_('User'))
+    billing_id = models.CharField(max_length=100, verbose_name=_('Billing ID'))
+    last_bill_image = models.FileField(
+        upload_to='images/', verbose_name=_('Last bill image'), blank=True, null=True)
+    national_card_image = models.FileField(
+        upload_to='images/', verbose_name=_('National card image'), blank=True, null=True)
+    ownership_doc_image = models.FileField(
+        upload_to='images/', verbose_name=_('Ownership document image'), blank=True, null=True)
+    date = models.DateField(verbose_name=_('Date'))
+    time = models.TimeField(verbose_name=_('Time'))
+    type_of_damage = models.ForeignKey(
+        TypeOfDamage, on_delete=models.SET_NULL, related_name='damage_reports',
+        blank=True, null=True, verbose_name=_('Type of damage')
+    )
+    description = models.TextField(verbose_name=_('Description'))
+    amount_of_damages = models.FloatField(verbose_name=_('Amount of damages'))
+    status = models.IntegerField(
+        choices=Status.choices, default=Status.TODO, verbose_name=_('Status'))
+    operator = models.ForeignKey(
+        Operator, on_delete=models.SET_NULL,
+        related_name='damage_reports', verbose_name=_('Operator'), blank=True, null=True
+    )
+    done_date = models.DateTimeField(
+        verbose_name=_('done at'), blank=True, null=True)
+    operator_description = models.TextField(verbose_name=_(
+        'Operator_Description'), blank=True, null=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('Created at'), blank=True, null=True)
+
+
+class AdditionalDocument(models.Model):
+    image = models.FileField(
+        upload_to='images/', verbose_name=_('Image'), blank=True, null=True)
+    damage_report = models.ForeignKey(
+        DamageReport, on_delete=models.CASCADE, related_name='additional_documents', verbose_name=_('Damage report')
+    )
--- damage/views.py
+++ damage/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from damage.models import DamageReport, TypeOfDamage
+from .serializer import (
+    CreateDamageReportSerializer, RetrieveDamageReportSerializer,
+    TypeOfDamageSerializer, ValidateDamageReportSerializer
+)
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.views import APIView
+from rest_framework import status
+from django.utils.translation import ugettext_lazy as _
 
-# Create your views here.
+
+class CreateDamageReportView(CreateAPIView):
+    serializer_class = CreateDamageReportSerializer
+    queryset = DamageReport.objects.all()
+
+
+class ListTypeOfDamagesView(ListAPIView):
+    serializer_class = TypeOfDamageSerializer
+    queryset = TypeOfDamage.objects.all()
+
+
+class RetrieveDamageReportView(APIView):
+    def post(self, request):
+        serializer = ValidateDamageReportSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            try:
+                damage_report = DamageReport.objects.get(
+                    billing_id=serializer.data['billing_id'],
+                    unique_id=serializer.data['unique_id']
+                )
+            except DamageReport.DoesNotExist:
+                return Response({
+                    'message': _('There is not damage report with given data.')
+                },
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            return Response(
+                RetrieveDamageReportSerializer(damage_report).data
+            )

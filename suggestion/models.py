b52205aeb13d07a0781e93cc98af5c16d406248f
sajjad <faniam321@gmail.com>
2022-08-21 17:34:00 +0430

    add model and serializer

--- suggestion/models.py
+++ suggestion/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Suggestion(models.Model):
+    full_name = models.CharField(max_length=200, verbose_name=_('Full name'))
+    email = models.EmailField(verbose_name=_('Email'))
+    phone_number = PhoneNumberField(verbose_name=_('Phone number'))
+    description = models.TextField(verbose_name=_('Description'))
+
+    def __str__(self) -> str:
+        return self.phone_number

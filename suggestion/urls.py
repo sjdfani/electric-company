60a9ebb155de2caf9baf546e5d8fc9120965389c
sajjad <faniam321@gmail.com>
2022-08-21 17:39:02 +0430

    complete suggestion

--- suggestion/urls.py
+++ suggestion/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import CreateSuggestionView
 
 app_name = 'suggestion'
 
 urlpatterns = [
+    path('suggestions/', CreateSuggestionView.as_view(), name='create-suggestion')
 
 ]

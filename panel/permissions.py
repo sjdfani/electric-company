f32da1779351cd454eb98f9ac1d40b2a567f171f
sajjad <faniam321@gmail.com>
2022-08-21 18:51:08 +0430

    add permissions

--- panel/permissions.py
+++ panel/permissions.py
@@ -4,3 +4,8 @@ from rest_framework.permissions import BasePermission
 class IsSuperuser(BasePermission):
     def has_permission(self, request, view):
         return (request.user and request.user.is_superuser)
+
+
+class IsAdminOrSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return ((request.user and request.user.is_staff) or (request.user and request.user.is_superuser))

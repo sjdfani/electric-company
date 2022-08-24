fc1ccc7fa0ea6e3f879c3c08f069e6fafecaaf2e
sajjad <faniam321@gmail.com>
2022-08-24 18:38:50 +0430

    remove extra fields and add user fields

--- suggestion/models.py
+++ suggestion/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import ugettext_lazy as _
+from users.models import User
 
 
 class Suggestion(models.Model):
-    full_name = models.CharField(max_length=200, verbose_name=_('Full name'))
-    email = models.EmailField(verbose_name=_('Email'))
-    phone_number = PhoneNumberField(verbose_name=_('Phone number'))
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name=_('Phone number'))
     description = models.TextField(verbose_name=_('Description'))
 
     def __str__(self) -> str:
-        return self.phone_number
+        return self.user.phone_number

7001da67c2daedb18836fb7dbc8ea9da2c38c7c9
sajjad <faniam321@gmail.com>
2022-08-24 18:47:59 +0430

    add admin

--- suggestion/admin.py
+++ suggestion/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Suggestion
 
-# Register your models here.
+
+class SuggestionAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    list_filter = ('user',)
+
+
+admin.site.register(Suggestion, SuggestionAdmin)

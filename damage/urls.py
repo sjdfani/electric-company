3687f18145eb33b4f4537499fba2c1a945d528c4
sajjad <faniam321@gmail.com>
2022-08-29 21:05:28 +0430

    remove extra url

--- damage/urls.py
+++ damage/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import (
-    CreateDamageReportView, ListTypeOfDamagesView, RetrieveDamageReportView
+    CreateDamageReportView, ListTypeOfDamagesView, RetrieveDamageReportView,
 )
 
 app_name = 'damage'
 
 urlpatterns = [
-    path('', CreateDamageReportView.as_view(), name='damage-reports'),
+    path('create/', CreateDamageReportView.as_view(), name='damage-reports'),
     path('type-of-damages/', ListTypeOfDamagesView.as_view(), name='type-of-damages'),
     path('track/', RetrieveDamageReportView.as_view(), name='track'),
 ]

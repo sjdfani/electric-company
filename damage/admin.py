e9209cb79df64edc065ec9e5d24b1fa84a525d3c
sajjad <faniam321@gmail.com>
2022-08-30 22:01:02 +0430

    complete admin

--- damage/admin.py
+++ damage/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import DamageReport
+from .models import DamageReport, TypeOfDamage
 
 
 class DamageReportAdmin(admin.ModelAdmin):
-    list_display = ('user',)
+    list_display = ('user', 'billing_id', 'date_time', 'done_date')
+    list_filter = ('user', 'type_of_damage', 'status')
+
+
+class TypeOfDamageAdmin(admin.ModelAdmin):
+    list_display = ('title',)
 
 
 admin.site.register(DamageReport, DamageReportAdmin)
+admin.site.register(TypeOfDamage, TypeOfDamageAdmin)

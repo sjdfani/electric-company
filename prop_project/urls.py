98617fb73f6a697cf203540ac5fb9651e70e833b
sajjad <faniam321@gmail.com>
2022-09-03 14:30:23 +0430

    fix bug

--- prop_project/urls.py
+++ prop_project/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,4 @@ urlpatterns = [
     path('api/v1/panel/', include('panel.urls')),
     path('api/v1/suggestion/', include('suggestion.urls')),
     path('api/v1/damage/', include('damage.urls')),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
